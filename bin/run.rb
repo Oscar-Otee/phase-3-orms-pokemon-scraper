881013513cc44835f2560b93b55ed462111e6fd4
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-24 17:04:54 +0300

    commit tree 4720a8adac9609447442a175e89739db826b4477

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,17 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+class SQLRunner
+    def initialize(db)
+      @db = db
+    end
+  
+    def execute_schema_migration_sql
+      sql = File.read('db/schema_migration.sql')
+      execute_sql(sql)
+    end
+  
+    def execute_sql(sql)
+       sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
+    end
+end
